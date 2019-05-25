32a4b33326acec9025171086919b5d804572c39f
Oohrahmaki <nettie.smith@rocketmail.com>
2019-05-25 16:41:06 +0100

    add function for formatting the string result

--- script.js
+++ script.js
@@ -17,8 +17,12 @@ const birthstoneMap = {
     december:"Turquoise"
 }
 
+function formatString(month, birthstone){
+    return "The birthstone for " + month.charAt(0).toUpperCase() + month.slice(1) +" is" + " " + birthstone;
+}
+
 button.addEventListener("click", function(){
     const month = inputDate.value;
     const birthstone = birthstoneMap[month];
-    result.innerHTML="The birthstone for " + month.charAt(0).toUpperCase() + month.slice(1) +" is" + " " + birthstone;
+    result.innerHTML= formatString(month, birthstone);
 });
